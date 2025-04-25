0e03841c61a99d1c6f59534886b493eb64a31c1c
YDZN26 <125135943+YDZN26@users.noreply.github.com>
2025-04-25 13:32:16 -0400

    correccion

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sis457_pizzeria.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
